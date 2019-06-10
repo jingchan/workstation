e5e6ffba3d0bce6ac907a377f5e1c9c5ec522a5d
Jing Chan <jingchan@gmail.com>
2019-06-10 07:49:45 +0000

    Change setup so Google cloud package repo only added once.

--- setup.sh
+++ setup.sh
@@ -5,3 +5,13 @@ apt update && apt upgrade
 apt install -y mosh
 apt install -y nodejs
 apt install -y npm
+
+
+# Install google cloud sdk
+CLOUD_SDK_REPO="cloud-sdk-$(grep VERSION_CODENAME /etc/os-release | cut -d '=' -f 2)"
+echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+#echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+apt-get install apt-transport-https ca-certificates
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update && sudo apt-get install google-cloud-sdk
+
