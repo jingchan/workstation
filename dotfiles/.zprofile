439dcc6ccd1ff5353edcae26886e079d80b19980
Jing Chan <jingchan@gmail.com>
2022-05-11 22:02:46 -0400

    Add mac setup script and zprofile file.

---
+++ dotfiles/.zprofile
@@ -0,0 +1,6 @@
+# Add `brew` to path.
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Out-out of analytics for Homebrew.
+export HOMEBREW_NO_ANALYTICS=1
+
---
+++ setup-mac.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Install nodeJS
+brew install node
