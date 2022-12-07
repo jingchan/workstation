195fb540762abdc3afe9e2573ca7c0eaf861f85f
Jing Chan <jingchan@gmail.com>
2022-12-06 23:05:43 -0500

    Edits.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Workstation Setup
+
+# To setup mac
+
+1. Setup terminal theme.
+git submodule init
+git submodule update
+Import Theme into terminal
+2. Run ./dotfiles/bootstrap.sh
+3. Run ./setup-mac.sh
--- setup-mac.sh
+++ setup-mac.sh
@@ -3,9 +3,10 @@
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+# Commenting out to see if we need this.
 # To resolve `zsh compinit: insecure directories` warnings
-chmod go-w '/opt/homebrew/share'
-chmod -R go-w '/opt/homebrew/share/zsh'
+#chmod go-w '/opt/homebrew/share'
+#chmod -R go-w '/opt/homebrew/share/zsh'
 
 # Install nodeJS
 # brew install node
@@ -14,6 +15,6 @@ chmod -R go-w '/opt/homebrew/share/zsh'
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Install bazel
-brew install bazelisk
+# brew install bazelisk
 
 
