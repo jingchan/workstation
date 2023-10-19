550fec8c895fb555da1628ecb886b3c49f5f1c6b
Jing Chan <jingchan@gmail.com>
2023-10-18 19:10:49 -0700

    Add adding user to docker in script.

--- install-scripts/install-docker.sh
+++ install-scripts/install-docker.sh
@@ -1,1 +1,4 @@
 curl -fsSL https://get.docker.com | bash
+sudo groupadd docker  # Create docker group.
+sudo usermod -aG docker $USER  # Add current user to docker group.
+newgrp docker  # Reload docker group.
